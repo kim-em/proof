293e843a679a8b1fb6713cda0d0ee3bbb77f0778
Scott Morrison <scott@tqft.net>
2016-06-30 01:08:49 +1000

    getting to monoidal categories. Inheritance is painful.

--- category-theory-type-classes.v
+++ category-theory-type-classes.v
@@ -1,12 +1,35 @@
 (* learn more about type classes *)
+Require Import CpdtTactics.
 
-Class Category { object: Type } { hom: object -> object -> Type } : Type := {
-  identity: forall a: object, hom(a)(a);
-  composition { a b c: object }:
-      hom(a)(b) -> hom(b)(c) -> hom(a)(c);
-  leftIdentities { a b: object }( f: hom(a)(b) ): composition(identity(a))(f) = f;
-  rightIdentities { a b: object }( f: hom(a)(b) ): composition(f)(identity(b)) = f;
-  associativity { a b c d: object }( f: hom(a)(b) )( g: hom(b)(c) )( h: hom(c)(d) ):
-    composition(composition(f)(g))(h) = composition(f)(composition(g)(h));
+Set Implicit Arguments.
+
+Class Category (object: Type) (hom: object -> object -> Type) := {
+  identity ( a: object ): hom a a;
+  composition { a b c: object }: hom a b -> hom b c -> hom a c;
+
+  leftIdentities { a b: object }( f: hom a b ): composition (identity a) f = f;
+  rightIdentities { a b: object }( f: hom a b ): composition f (identity b) = f;
+  associativity { a b c d: object }( f: hom a b )( g: hom b c )( h: hom c d ):
+    composition (composition f g) h = composition f (composition g h);
 }.
 
+Print Category.
+
+Program Definition NaturalsAsCategory: Category (fun (a b: True) => nat) := {|
+  identity := fun _ => 0;
+  composition := fun _ _ _ f g => f + g;
+|}.
+Next Obligation.
+  crush.
+Defined.
+
+Generalizable Variables sourceObject sourceHom targetObject targetHom.
+
+Class Functor `{ source: Category sourceObject sourceHom } `{ target: Category targetObject targetHom } := {
+  onObjects: sourceObject -> targetObject;
+  onMorphisms { x y: sourceObject }: sourceHom x y -> targetHom (onObjects x) (onObjects y);
+  identities( x: sourceObject):
+    onMorphisms(identity source x) = target.(identity)(onObjects(x));
+  functoriality { x y z: source.(object) }( f: source.(hom)(x)(y) )( g: source.(hom)(y)(z)):
+      onMorphisms(source.(composition)(f)(g)) = target.(composition)(onMorphisms(f))(onMorphisms(g))
+}.
--- category-theory.v
+++ category-theory.v
@@ -4,31 +4,50 @@ Set Implicit Arguments.
 Structure Category := {
   object: Type;
   hom: object -> object -> Type;
-  identity ( a: object ): hom(a)(a);
-  composition { a b c: object }: hom(a)(b) -> hom(b)(c) -> hom(a)(c);
-  leftIdentities { a b: object }( f: hom(a)(b) ): composition(identity(a))(f) = f;
-  rightIdentities { a b: object }( f: hom(a)(b) ): composition(f)(identity(b)) = f;
-  associativity { a b c d: object }( f: hom(a)(b) )( g: hom(b)(c) )( h: hom(c)(d) ):
-    composition(composition(f)(g))(h) = composition(f)(composition(g)(h));
+  identity ( a: object ): hom a a;
+  composition { a b c: object }: hom a b -> hom b c -> hom a c;
+  leftIdentities { a b: object }( f: hom a b ): composition (identity a) f = f;
+  rightIdentities { a b: object }( f: hom a b ): composition f (identity b) = f;
+  associativity { a b c d: object }( f: hom a b )( g: hom b c )( h: hom c d ):
+    composition (composition f g) h = composition f (composition g h);
 }.
 
+Program Definition NaturalsAsCategory: Category := {|
+  object := True;
+  hom := fun _ _ => nat;
+  identity := fun _ => 0;
+  composition := fun _ _ _ f g => f + g;
+|}.
+Next Obligation.
+  crush.
+Defined.
+
 Structure Functor(source target: Category) := {
-  onObjects: source.(object) -> target.(object);
-  onMorphisms { x y: source.(object) }:
-    source.(hom)(x)(y) -> target.(hom)(onObjects(x))(onObjects(y));
-  identities( x: source.(object) ):
-    onMorphisms(source.(identity)(x)) = target.(identity)(onObjects(x));
-  functoriality { x y z: source.(object) }( f: source.(hom)(x)(y) )( g: source.(hom)(y)(z)):
-      onMorphisms(source.(composition)(f)(g)) = target.(composition)(onMorphisms(f))(onMorphisms(g))
+  onObjects: object source -> object target;
+  onMorphisms { x y: object source }:
+    hom source x y -> hom target (onObjects x) (onObjects y);
+  identities( x: object source ):
+    onMorphisms (identity source x) = identity target (onObjects x);
+  functoriality { x y z: object source }( f: hom source x y )( g: hom source y z):
+      onMorphisms (composition source f g) = composition target (onMorphisms f) (onMorphisms g)
 }.
+
+Program Definition DoublingAsFunctor: Functor NaturalsAsCategory NaturalsAsCategory := {|
+  onObjects := fun(a: True) => a;
+  onMorphisms := fun _ _ x => 2 * x;
+  functoriality := _
+|}.
+Next Obligation.
+  crush.
+Defined.
 
 (* Can we use pattern matching in the arguments, instead of writing fst and snd everywhere? *)
 
 Program Definition CartesianProduct(C: Category)(D: Category): Category := {|
-  object := (C.(object) * D.(object)) % type;
+  object := (object C * object D) % type;
   hom := fun p q => ((hom C (fst p) (fst q)) * (hom D (snd p) (snd q))) % type;
   identity := fun p => (identity C (fst p), identity D (snd p));
-  composition := fun _ _ _ f g => (C.(composition) (fst f) (fst g), D.(composition) (snd f) (snd g));
+  composition := fun _ _ _ f g => (composition C (fst f) (fst g), composition D (snd f) (snd g));
   leftIdentities := _;
   rightIdentities := _;
   associativity := _
@@ -54,17 +73,14 @@ Program Definition castObject { C D: Category } ( Q: C = D ) ( a: C.(object) ) :
 Program Definition castMorphism { C D: Category } ( Q: C = D ) { x y: C.(object) } ( a: C.(hom) x y ) : D.(hom) (castObject Q x) (castObject Q y) := _.
 *)
 
-(* of course, this isn't the full definition of lax; we need an associativity constraint still! *)
-(* to define the strong case, we need to go back and talk about identities and isomorphisms *)
-
 Structure LaxMonoidalCategory := {
-  underlying: Category;
+  underlying :> Category;
   tensor: Functor (CartesianProduct underlying underlying) underlying;
-  associator(x y z: underlying.(object)):
+  associator(x y z: object underlying):
     underlying.(hom)(
-      (tensor.(onObjects)((tensor.(onObjects)(x,y)), z))
+      onObjects tensor ((onObjects tensor (x,y)), z)
     )(
-      (tensor.(onObjects)(x, (tensor.(onObjects)(y,z))))
+      (onObjects tensor (x, onObjects tensor (y,z)))
     );
 (*  pentagon:
     forall w x y z: underlying.(object),
@@ -78,18 +94,37 @@ Structure LaxMonoidalCategory := {
     ); *)
 }.
 
-(* getting ready for strong monoidal categories *)
+Program Definition TensorProductOfNaturals: Functor (CartesianProduct NaturalsAsCategory NaturalsAsCategory) NaturalsAsCategory := {|
+  onObjects := fun a => fst a;
+  onMorphisms := fun _ _ p => (fst p) + (snd p);
+|}.
+Next Obligation.
+  crush.
+Defined.
 
-(*
-Structure Isomorphism(category: Category)(source target: object category) := {
-  map: hom category source target;
-  inverse: hom category target source;
-  compositionIdentity1: composition category map inverse = identity category source;
-  compositionIdentity2: composition category inverse map = identity category target;
-}.*)
+Eval compute in onMorphisms TensorProductOfNaturals (2,7).
+
+Program Definition NaturalsAsLaxMonoidalCategory: LaxMonoidalCategory := {|
+  underlying := NaturalsAsCategory;
+  tensor := TensorProductOfNaturals;
+  associator := fun(x y z: True) => 0;
+|}.
+
 
 Structure Inverse { category: Category } { source target: object category } ( map: hom category source target ) := {
   inverse: hom category target source;
   rightInverseIdentity: composition category map inverse = identity category source;
   leftInverseIdentity : composition category inverse map = identity category target;
 }.
+
+Structure MonoidalCategory := {
+  underlyingLax :> LaxMonoidalCategory;
+  strength(x y z : object (underlying underlyingLax)): Inverse(associator underlyingLax x y z);
+}.
+
+Program Definition NaturalsAsMonoidalCategory: MonoidalCategory := {|
+  underlyingLax := NaturalsAsLaxMonoidalCategory;
+  strength := _;
+|}.
+Next Obligation.
+Admitted.
