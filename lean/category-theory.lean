6a4a4aad72da9ed8ff0f9bcae4308c9af7e4b0a6
Scott Morrison <scott@tqft.net>
2017-01-04 10:14:37 +1100

    stuff

--- lean/category-theory.lean
+++ lean/category-theory.lean
@@ -1,67 +1,67 @@
 import standard
 
-structure Category : Type :=
+
+structure Category :=
   (Obj : Type)
   (Hom : Obj → Obj → Type)
   
-  (Id : Π A : Obj, Hom A A)
-  (compose : Π ⦃A B C : Obj⦄, Hom B C → Hom A B → Hom A C)
+  (identity : Π A : Obj, Hom A A)
+  (compose : Π ⦃A B C : Obj⦄, Hom A B → Hom B C → Hom A C)
 
-  (Id_left  : Π ⦃A B : Obj⦄ (f : Hom A B), compose !Id f = f)
-  (Id_right : Π ⦃A B : Obj⦄ (f : Hom A B), compose f !Id = f)
-  (assoc : Π ⦃A B C D : Obj⦄ (f : Hom C D) (g : Hom B C) (h : Hom A B),
+  (left_identity  : Π ⦃A B : Obj⦄ (f : Hom A B), compose (identity A) f = f)
+  (right_identity : Π ⦃A B : Obj⦄ (f : Hom A B), compose f (identity B) = f)
+  (associativity  : Π ⦃A B C D : Obj⦄ (f : Hom A B) (g : Hom B C) (h : Hom C D),
     compose (compose f g) h = compose f (compose g h))
 
 namespace Category
   -- Can we put this before the definition?
   notation f ∘ g := compose _ _ _ _ f g
-  --infixr `∘` := compose _ _ _ _
+  -- infixr `∘` := compose _ _ _ _
   infixl `⟶` :25 := Hom _
-  --definition Mor := Hom
   variables C : Category
   variables {a b c d : Obj C} 
 end Category
 
 open Category
-open nat
 
 definition ℕCategory : Category :=
-  ⦃ Category,
-     Obj     := unit,
-     Hom     := λ A B, ℕ,
-     Id      := λ A, 0,
-     compose := λ A B C, add,
+  { Category .
+     Obj      := unit,
+     Hom      := λ A B, ℕ,
+     identity := λ A, 0,
+     compose  := λ A B C, add, 
 
-     Id_left  := begin
-                   intros,
-                   generalize f,
-                   exact zero_add
-                 end,
-     Id_right := λ A B, add_zero,
-     assoc    := λ A B C D, nat.add_assoc ⦄
+     left_identity  := begin
+                         intros
+                         generalize f,
+                         exact zero_add
+                       end,
+     right_identity := λ A B, add_zero,
+     associativity    := λ A B C D, add_assoc }
 
-structure Functor (source target : Category) : Type :=
-  (onObj : Obj source → Obj target)
-  (onMor : Π ⦃A B : Obj source⦄, Hom source A B → Hom target (onObj A) (onObj B))
+structure Functor (source target : Category) :=
+  (onObjects     : Obj source → Obj target)
+  (onMorphisms   : Π ⦃A B : Obj source⦄, Hom source A B → Hom target (onObjects A) (onObjects B))
   
-  (respect_Id   : Π (A : Obj source), onMor (Id source A) = Id target (onObj A))
-  (respect_comp : Π ⦃A B C : Obj source⦄ (f : Hom source A B) (g : Hom source B C),
-                    onMor (compose source g f)
-                     = compose target (onMor g) (onMor f))
+  (identities    : Π (A : Obj source), onMorphisms (identity source A) = identity target (onObjects A))
+  (functoriality : Π ⦃A B C : Obj source⦄ (f : Hom source A B) (g : Hom source B C),
+                    onMorphisms (compose source f g)
+                     = compose target (onMorphisms f) (onMorphisms g))
 
-definition double : ℕ → ℕ
-| double x := x + x
+definition double(x : ℕ) := 2 * x
 
 definition distribu : Π (n m : ℕ), (n + m) + (n + m) = (n + n) + (m + m) :=
   sorry
 
 definition DoublingAsFunctor : Functor ℕCategory ℕCategory :=
-  ⦃ Functor,
-    onObj := id,
-    onMor := λ A B, double,
+  { 
+    onObjects := id,
+    onMorphisms := λ A B, double,
 
-    respect_Id   := λ A, rfl,
-    respect_comp := sorry
+    identities   := begin
+                      intros
+                    end,
+    functoriality := sorry
       --begin
       --  intro A B C f g,
       --  cases f,
@@ -70,19 +70,19 @@ definition DoublingAsFunctor : Functor ℕCategory ℕCategory :=
       --  apply rfl,
       --  cases g,
       --end
-      ⦄
+  }
 
 open prod
 
 definition CartesianProduct (C D : Category) : Category :=
-  ⦃ Category,
+  { Category .
     Obj := Obj C × Obj D,
     Hom := λ A B, Hom C (pr1 A) (pr1 B) × Hom D (pr2 A) (pr2 B),
     --Hom := λ A B, match (A, B) with
     --         ((A₁, A₂), (B₁, B₂)) := Hom C A₁ B₁ × Hom D A₂ B₂
     --         end,
 
-    Id  := λ A, match A with
+    identity  := λ A, match A with
              (A₁, A₂) := (Id C A₁, Id D A₂)
            end,
     compose := λ A B E f g, (compose A (pr1 f) (pr1 g), compose B (pr2 f) (pr2 g)),
@@ -92,6 +92,6 @@ definition CartesianProduct (C D : Category) : Category :=
                  --end
                --end,
 
-    Id_left  := sorry,
-    Id_right := sorry,
-    assoc    := sorry ⦄
+    left_identity    := sorry,
+    right_identity   := sorry,
+    associativity    := sorry ⦄
