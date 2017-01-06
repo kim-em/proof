e5ad303af87eaa1648c4768a93dfcb0760686fd6
Scott Morrison <scott@tqft.net>
2017-01-06 11:58:54 +1100

    modifying cat-param.lean, so things are not quite as parametrised

--- lean/cat-param.lean
+++ lean/cat-param.lean
@@ -1,9 +1,3 @@
-/- This is the parameterised implementation of Category. It has the
-   disadvantage that we're carrying around a mess of ugly
-   parameters. This could be mitigated by defining some convenience
-   structures. This is the approach taken in the Lean 2 standard
-   library, except with only the objects as a parameter. -/
-
 import standard
 
 -- Oh, there it is.
@@ -23,25 +17,27 @@ instance ℕCategory : Category unit (λ a b, ℕ) :=
   { identity := λ a, 0,
     compose  := λ a b c, add,
 
-    left_identity  := λ a b, zero_add,
-    right_identity := λ a b, add_zero,
-    associativity  := λ a b c d, add_assoc
+    left_identity  := λ a b, zero_add, -- sadly, "by blast" doesn't work here
+    right_identity := by blast, --λ a b, add_zero,
+    associativity  := by blast --λ a b c d, add_assoc
   }
 
 open Category
 
-class Functor (Obj₁ : Type) (Hom₁ : Obj₁ → Obj₁ → Type) [C₁ : Category Obj₁ Hom₁]
-              (Obj₂ : Type) (Hom₂ : Obj₂ → Obj₂ → Type) [C₂ : Category Obj₂ Hom₂]
-              (onObjects   : Obj₁ → Obj₂)
-              (onMorphisms : Π ⦃A B : Obj₁⦄,
-                Hom₁ A B → Hom₂ (onObjects A) (onObjects B)) :=
+class Functor { Obj₁ : Type } { Hom₁ : Obj₁ → Obj₁ → Type } (C₁ : Category Obj₁ Hom₁)
+              { Obj₂ : Type } { Hom₂ : Obj₂ → Obj₂ → Type } (C₂ : Category Obj₂ Hom₂) :=
+  (onObjects   : Obj₁ → Obj₂)
+  (onMorphisms : Π ⦃A B : Obj₁⦄,
+                Hom₁ A B → Hom₂ (onObjects A) (onObjects B))
   (identities : Π (A : Obj₁),
     onMorphisms (identity _ A) = identity _ (onObjects A))
   (functoriality : Π ⦃A B C : Obj₁⦄ (f : Hom₁ A B) (g : Hom₁ B C),
     onMorphisms (compose _ f g) = compose _ (onMorphisms f) (onMorphisms g))
   
-instance DoublingAsFunctor : Functor unit (λ A B, ℕ) unit (λ A B, ℕ)
-                               id (λ A B n, n + n) :=
-  { identities    := by blast,
+instance DoublingAsFunctor : Functor ℕCategory ℕCategory :=
+  { 
+    onObjects     := id,
+    onMorphisms   := (λ A B n, n + n), -- sadly, "by blast" doesn't work below if we replace "n + n" with "2 * n"
+    identities    := by blast,
     functoriality := by blast
   }
