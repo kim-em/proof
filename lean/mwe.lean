b1bfaa2f1b9c36a5bdfad41de1579063c06d2676
Scott Morrison <scott@tqft.net>
2017-01-06 17:12:28 +1100

    code from lean-user post

--- lean/mwe.lean
+++ lean/mwe.lean
@@ -1,24 +1,47 @@
-structure Category :=
-  (Obj : Type)
-  (Hom : Obj → Obj → Type)
+section bundled
+  structure Category_bundled :=
+    (Obj : Type)
+    (Hom : Obj → Obj → Type)
 
-print Category
-/-
-5:1: structure Category : Type₂
-fields:
-Category.Obj : Category → Type
-Category.Hom : Π (c : Category), Category.Obj c → Category.Obj c → Type
--/
+  open Category_bundled
 
-class Category' :=
-  (Obj : Type) 
-  (Hom : Obj → Obj → Type)
+  structure Functor_bundled (source target: Category_bundled) :=
+    (onObjects: Obj source -> Obj target)
+    (onMorphisms: Π { A B : Obj source }, Hom source A B -> Hom target (onObjects A) (onObjects B))
+end bundled
 
-print Category'
-/-
-17:1: attribute [class]
-structure Category' : Type₂
-fields:
-Category'.Obj : Π [c : Category'], Type
-Category'.Hom : Π [c : Category'], Category'.Obj → Category'.Obj → Type
--/
+section parameterized
+  structure Category_parameterized
+    (Obj : Type)
+    (Hom : Obj → Obj → Type)
+
+  structure Functor_parameterized
+    { Obj1 : Type } { Hom1 : Obj1 -> Obj1 -> Type } ( source : Category_parameterized Obj1 Hom1 )
+    { Obj2 : Type } { Hom2 : Obj2 -> Obj2 -> Type } ( target : Category_parameterized Obj2 Hom2 ) :=
+    (onObjects: Obj1 -> Obj2 )
+    (onMorphisms: Π { A B : Obj1 }, Hom1 A B -> Hom2 (onObjects A) (onObjects B))
+end parameterized
+
+section bundled_class
+  class Category_bundled_class :=
+    (Obj : Type)
+    (Hom : Obj → Obj → Type)
+
+  open Category_bundled_class
+
+  structure Functor_bundled_class (source target: Category_bundled_class) :=
+    (onObjects: @Obj source -> @Obj target)
+    (onMorphisms: Π { A B : @Obj source }, @Hom source A B -> @Hom target (onObjects A) (onObjects B))
+end bundled_class
+
+section parameterized_class
+  class Category_parameterized_class
+    (Obj : Type)
+    (Hom : Obj → Obj → Type)
+
+  structure Functor_parameterized_class
+    { Obj1 : Type } { Hom1 : Obj1 -> Obj1 -> Type } ( source : Category_parameterized_class Obj1 Hom1 )
+    { Obj2 : Type } { Hom2 : Obj2 -> Obj2 -> Type } ( target : Category_parameterized_class Obj2 Hom2 ) :=
+    (onObjects: Obj1 -> Obj2 )
+    (onMorphisms: Π { A B : Obj1 }, Hom1 A B -> Hom2 (onObjects A) (onObjects B))
+end parameterized_class
