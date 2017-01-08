71207e589779e7268c60a3c92a2a9bf87a958b05
Scott Morrison <scott@tqft.net>
2017-01-08 20:33:50 +1100

    Updating mwe.lean, showing use of coercions, per Leonardo's post at https://groups.google.com/forum/#!topic/lean-user/VrkcyKxrXps

--- lean/mwe.lean
+++ lean/mwe.lean
@@ -1,6 +1,9 @@
-class C1 :=
-  (a: unit)
-class C2 extends C1
+structure C1 := (a: unit)
+structure C2 extends C1
+
+attribute [class] C1 C2
+instance : has_coe C2 C1 := ⟨C2.to_C1⟩
+instance C2_to_C1 [ c2: C2 ] : C1 := c2
 
 definition X1: C1 := { a := unit.star }
 definition X2: C2 := { a := unit.star }
