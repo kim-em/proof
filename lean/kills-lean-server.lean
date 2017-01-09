c240a2da9735417cd55f23f63265afb2bed6aa1b
Scott Morrison <scott@tqft.net>
2017-01-10 08:12:51 +1100

    example of the bug reported in https://github.com/leanprover/lean/issues/1290

---
+++ lean/kills-lean-server.lean
@@ -0,0 +1,10 @@
+structure Category :=
+  (Obj : Type)
+  (Hom : Obj -> Obj -> Type)
+
+structure Isomorphism ( C: Category ) { A B : C^.Obj } :=
+  (morphism : C^.Hom A B)
+
+instance Isomorphism_coercion_to_morphism { C : Category } { A B C^.Obj } : has_coe (Isomorphism C A B) (C^.Hom A B) :=
+  (coe: Isomorphism.morphism)
+
